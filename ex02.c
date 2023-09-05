d25b27212d325b242fafe3a2cc9fe8f12887c345
Roberto Padilha <robertocesardasilva@gmail.com>
2023-09-05 20:29:07 -0300

    Aula 05/09

--- ex02.c
+++ ex02.c
@@ -4,12 +4,14 @@
 typedef struct Item {
     int valor;
     struct Item* proximo;
+    struct Item* anterior;
 } Item;
 
 Item* criarItem(int valor) {
     Item* novoItem = malloc(sizeof(Item));
     novoItem->valor = valor;
     novoItem->proximo = NULL;
+    novoItem->anterior = NULL;
 
     return novoItem;
 }
@@ -26,6 +28,7 @@ void adicionarItem(Item** inicio, int valor) {
         }
 
         atual->proximo = novoItem;
+        novoItem->anterior = atual;
     }
 }
 
@@ -49,11 +52,20 @@ void exibirLista(Item** inicio) {
     }
 }
 
+void exibirListaReversa(Item** final) {
+    Item* item = *final;
+    while (item != NULL) {
+        printf("%d \n", item->valor);
+        item = item->anterior;
+    }
+}
+
 void removerItem(Item** inicio, int pos) {
     Item* atual = *inicio;
 
     if (pos == 0) {
         *inicio = atual->proximo;
+        (*inicio)->anterior = NULL;
         free(atual);
         return;
     }
@@ -70,6 +82,8 @@ void removerItem(Item** inicio, int pos) {
         Item* novoProximo = itemRemover->proximo;
 
         atual->proximo = novoProximo;
+        novoProximo->anterior = atual;
+
         free(itemRemover);
     }
 }
@@ -85,13 +99,17 @@ int main() {
     exibirLista(&inicio);
 
     Item* item = obterItem(&inicio, 2);
-    printf("A posicao 2 da lista tem o valor: %d", item->valor);
+    printf("A posicao 2 da lista tem o valor: %d\n", item->valor);
 
     removerItem(&inicio, 0);
     printf("Removendo item da posição 0\n");
     printf("Lista após remoção: \n");
 
     exibirLista(&inicio);
+
+    Item* ultimo = obterItem(&inicio, 3); // obtém o ultimo item da lista
+    printf("Exibindo a lista reversa: \n");
+    exibirListaReversa(&ultimo);
 
     return 0;
 }
